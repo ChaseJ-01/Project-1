f35f75f7f10344e878c7ac59a4c92901aee4f5e9
ChaseJ-01 <chase.jones@revature.net>
2021-09-02 16:41:09 -0400

    Second commit

---
+++ src/main/java/com/project1/servlets/TableServlet.java
@@ -0,0 +1,10 @@
+package com.project1.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class TableServlet extends HttpServlet{
+
+}
